02368d748f69d39922af7a926b30ff5c71584d4e
stephentetley <stephen.tetley@gmail.com>
2019-08-17 20:21:28 +0100

    Added missing file.

---
+++ src/SLSqlite/Utils.fs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Stephen Tetley 2019
+// License: BSD 3 Clause
+
+namespace SLSqlite
+
+
+module Utils =
+    
+    open System
+
+    open System.Data.SQLite
+
+    // SQLite has no native date type, represent data times as strings
+    // in ISO 8601 format
+
+    let toIso8601String (dt : DateTime) : string = 
+        dt.ToString(format = "yyyy-MM-ddThh:mm:ss")
+
+    let parseIso8601String (source : string) : DateTime = 
+        DateTime.ParseExact(s = source, format = "yyyy-MM-ddThh:mm:ss", provider = Globalization.CultureInfo.InvariantCulture)
+
+    let tryParseIso8601String (source : string) : DateTime option = 
+        try
+            parseIso8601String source |> Some
+        with
+        | _ -> None
+
+
+    /// Note - favour Parametrized Queries whci removes the need for this
+    let escapeQuotes (source: string) : string = 
+        source.Replace("'", "''")
+ 
+    // Note - there is plenty of scope for a secondary interface
+    // automating the command line: e.g.
+    // > sqlite3 change_request.db ".output cr_schema.sql" ".schema" ".exit"
+    //
+    // This functionality is not availiable through System.Data.SQLite
+
+
+    /// Using SQLite's SQLiteDataReader directly is too often permissive 
+    /// as it grants access to the traversal and the current row.
+    /// Potentially we want to wrap SQLiteDataReader and just expose 
+    /// the row reading functions.
+    [<Struct>]
+    type RowReader = 
+        | RowReader of SQLiteDataReader
+
+        member x.GetString(i : int) : string = 
+            match x with
+            | RowReader reader -> reader.GetString(i)
